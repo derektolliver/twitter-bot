bed95ea930329d605abff2c1327a4acb185255ea
Juan M Penaranda <jmpo1618@gmail.com>
2016-10-15 16:35:02 -0500

    Added ability to read tweets from search.

--- harambot.py
+++ harambot.py
@@ -7,4 +7,8 @@ auth = tweepy.OAuthHandler(twitter_info.T['consumer_key'], twitter_info.T['consu
 auth.set_access_token(twitter_info.T['access_token'], twitter_info.T['access_secret'])
 
 api = tweepy.API(auth)
-api.update_status('Hope this works from heaven #immortality')
+# api.update_status('Hope this works from heaven #immortality')
+
+for tweet in tweepy.Cursor(api.search, q='harambe').items():
+    print(tweet.text)
+    print('#############################################')
