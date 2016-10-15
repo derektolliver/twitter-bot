25c76af9029d3277d6508c3b1b69ee73c4c9cbfa
Juan M Penaranda <jmpo1618@gmail.com>
2016-10-15 16:05:17 -0500

    API setup, closes #2.

---
+++ harambot.py
@@ -0,0 +1,10 @@
+import tweepy
+import twitter_info
+
+
+auth = tweepy.OAuthHandler(twitter_info.T['consumer_key'], twitter_info.T['consumer_secret'])
+
+auth.set_access_token(twitter_info.T['access_token'], twitter_info.T['access_secret'])
+
+api = tweepy.API(auth)
+api.update_status('Hope this works from heaven #immortality')
