b18d000a017f88527b1c46a92808297e56be6942
Derek <derekt1026@gmail.com>
2016-10-15 20:46:34 -0500

    Added while loop to tweet as long as desired, also logging time for knowledge of next tweet

--- harambot.py
+++ harambot.py
@@ -2,7 +2,7 @@ from enum import Enum
 import tweepy
 from trainer import Trainer
 import twitter_info
-import random
+import random, time
 
 
 class tweet_action(Enum):
@@ -17,15 +17,21 @@ auth.set_access_token(twitter_info.T['access_token'],
                       twitter_info.T['access_secret'])
 api = tweepy.API(auth)
 
-t = Trainer(api)
-action = random.randint(1, 4)
-t.run_trainer(action)
+while True:
+    t = Trainer(api)
+    action = random.randint(1, 4)
+    t.run_trainer(action)
 
-if action == tweet_action.retweet:
-    api.retweet(t.id)
-elif action == tweet_action.quote:
-    api.update_status(t.tweet + " " + t.url)
-else:
-    print('TWEET:')
-    print(t.tweet)
-    api.update_status(t.tweet)
+    if action == tweet_action.retweet:
+        api.retweet(t.id)
+    elif action == tweet_action.quote:
+        api.update_status(t.tweet + " " + t.url)
+    else:
+        print('TWEET:')
+        print(t.tweet)
+        api.update_status(t.tweet)
+
+    # Sleep before next tweet
+    sleep_time = random.randint(60, 600)
+    print("time since last tweet:  " + time.ctime + ", sleep time: " + sleep_time + ", next tweet time: " time.ctime(time.time + sleep_time))
+    time.sleep(sleep_time)
